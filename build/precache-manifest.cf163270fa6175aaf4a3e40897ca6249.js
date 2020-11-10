7672b414619c2e6d77d612f49349566a5baefe9f
wangruobing <1400121920@qq.com>
2020-11-10 11:16:15 +0800

    wangruobing commit for react task5

---
+++ build/precache-manifest.cf163270fa6175aaf4a3e40897ca6249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df57d843421f07a8c84038da30b6f86e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad8aa85bffd7f95e9898",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "2ca84675f0b3093c3bda",
+    "url": "/static/css/main.ed61b727.chunk.css"
+  },
+  {
+    "revision": "ad8aa85bffd7f95e9898",
+    "url": "/static/js/2.9ebb12db.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.9ebb12db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca84675f0b3093c3bda",
+    "url": "/static/js/main.39eab2ab.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
